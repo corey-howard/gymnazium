8b6efaf618485e6c6f1c92b39a2dc22d1e85e30f
corey-howard <coreyhoward@live.co.uk>
2021-04-04 10:09:04 +0000

    Second stage of deployment to heroku - installed gunicorn and added hostname

--- gymnazium/settings.py
+++ gymnazium/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'gob6-1qu1z97*o3407i&0vd8%0(gd@of*i#**4_dej$*()8vvx'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['gymnazium.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -121,12 +121,17 @@ WSGI_APPLICATION = 'gymnazium.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
