9760015a6e7e327631cac2cc1e37ef1be3727bbf
corey-howard <coreyhoward@live.co.uk>
2021-04-04 11:30:46 +0000

    Removed secret key and set debug

--- gymnazium/settings.py
+++ gymnazium/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'gob6-1qu1z97*o3407i&0vd8%0(gd@of*i#**4_dej$*()8vvx'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['gymnazium.herokuapp.com', 'localhost']
 
