5848b2d0e696eb090066184b4a64051622013395
corey-howard <coreyhoward@live.co.uk>
2021-04-12 10:45:49 +0000

    Linked contact form to submit, messages now appear within the admin to see, and success message added for user to see once message submitted

--- contact/views.py
+++ contact/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.contrib import messages
 from .models import Contact
 
 
@@ -11,13 +11,15 @@ def contact(request):
 
     if request.method == "POST":
 
-        name = request.POST["name"]
-        email = request.POST["email"]
-        phone = request.POST["phone"]
-        message = request.POST["message"]
+        name = request.POST['name']
+        email = request.POST['email']
+        phone = request.POST['phone']
+        message = request.POST['message']
 
         contact = Contact(name=name, email=email, phone=phone, message=message)
 
         contact.save()
+
+        messages.success(request, 'Thank you for your message, we will get back to you as soon as possible.')
 
     return render(request, "contact/contact.html")
