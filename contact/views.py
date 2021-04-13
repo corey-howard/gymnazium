16601cb9889b4c232f3b798123ef8d515b75aa4f
corey-howard <coreyhoward@live.co.uk>
2021-04-13 10:32:53 +0000

    Added email logic to contact views to send email to site owner when user sends message

--- contact/views.py
+++ contact/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib import messages
 from .models import Contact
+from django.core.mail import send_mail
 
 
 def contact(request):
@@ -19,6 +20,16 @@ def contact(request):
         contact = Contact(name=name, email=email, phone=phone, message=message)
 
         contact.save()
+
+        # Send email
+        send_mail(
+            'You have been sent a message via Gone Fishing.',
+            'The message was sent from ' + name
+            + '. View this message in the admin.',
+            'EMAIL_HOST_USER',
+            ['sales@gymnazium.co.uk', 'coreyhoward@live.co.uk'],
+            fail_silently=False
+        )
 
         messages.success(request, 'Thank you for your message, we will get back to you as soon as possible.')
 
